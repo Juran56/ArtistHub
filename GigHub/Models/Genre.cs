6a6c1485506e6e317c2047e5041f0dd56b765181
Juran56 <juran56@gmail.com>
2018-07-24 17:58:44 +0600

    3rd state Update gig, genre Model

--- GigHub/Models/Genre.cs
+++ GigHub/Models/Genre.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
